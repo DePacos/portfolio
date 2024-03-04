a851009c4bb9cbcfa56ba9c41f37de2a194f6c68
DePacos <depacos@yandex.ru>
2024-03-04 19:06:22 +0300

    added SkillsSection Component, delete styles StyledMain

--- src/layout/sections/Main.tsx
+++ src/layout/sections/Main.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styled from "styled-components";
 import {MainSection} from "./mainSection/MainSection";
+import {SkillsSection} from "./skillsSection/SkillsSection";
 
 export function Main() {
     return (
         <StyledMain>
             <MainSection/>
+            <SkillsSection/>
         </StyledMain>
     );
 }
 
 const StyledMain = styled.main
     `
-        background: linear-gradient(90deg, rgba(36, 51, 92, 1) 30%, rgba(255, 255, 255, 0) 30%);
-        height: 100vh;
     `
