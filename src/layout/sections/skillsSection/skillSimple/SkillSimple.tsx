7fb183ba851858b97094c93e4954b30899a5641e
DePacos <depacos@yandex.ru>
2024-03-04 19:10:24 +0300

    create Component SkillsSimple

---
+++ src/layout/sections/skillsSection/skillSimple/SkillSimple.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../../../../components/icons/Icon";
+
+export function SkillSimple(props: { skillItems: Array<any> }) {
+
+    return (
+        <StyledSkillSimple>
+            {props.skillItems.map((e) => {
+
+                let iconSize = '100'
+                let iconViewBox = '0 0 120 120'
+
+                if(e.iconId === 'styledComponents'){iconViewBox = '0 0 38 38'}
+                if(e.iconId === 'tsIcon'){iconViewBox = '-25 -25 170 170'}
+                if(e.iconId === 'jsIcon'){iconViewBox = '-15 -15 150 150'}
+                if(e.iconId === 'githubIcon'){iconViewBox = '0 0 30 30'}
+                if(e.iconId === 'scssIcon'){iconViewBox = '-10 -10 120 120'}
+                if(e.iconId === 'gitIcon'){iconViewBox = '-10 -10 120 120'}
+                if(e.iconId === 'reactIcon'){iconViewBox = '-5 -10 120 120'}
+                if(e.iconId === 'reduxIcon'){iconViewBox = '-5 0 120 120'}
+
+                return (
+                    <li>
+                        <Icon iconId={e.iconId} size={iconSize} viewBox={iconViewBox}/>
+                        <span>{e.name}</span>
+                    </li>
+                )
+            })}
+        </StyledSkillSimple>
+    );
+}
+
+const StyledSkillSimple = styled.ul
+    `
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        justify-items: center;
+        text-align: center;
+
+        li {
+            margin-bottom: 60px;
+        }
+
+        span {
+            font-family: Roboto-Bold;
+            font-size: 18px;
+            line-height: 27px;
+            text-transform: uppercase;
+            display: grid;
+            align-items: center;
+            width: 161px;
+            height: 91px;
+            border-radius: 24px;
+            padding-top: 22px;
+            background-color: ${({theme}) => theme.colors.bgIcon};
+        }
+
+        svg {
+            width: max-content;
+            background-color: #173f5f;
+            border-radius: 50%;
+            border: 10px solid #222;
+            position: relative;
+            padding: 10px;
+            bottom: -30px;
+        }
+    `
