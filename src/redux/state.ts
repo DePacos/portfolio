9356fd33ccfe53261196a3d84b6a433f605a611c
DePacos <depacos@yandex.ru>
2024-03-06 00:02:22 +0300

    create data file

---
+++ src/redux/state.ts
@@ -0,0 +1,21 @@
+export const dataBase = {
+    menuData: [
+        {id: 1, name: 'Home', link: '#'},
+        {id: 2, name: 'Skills', link: '#'},
+        {id: 3, name: 'Projects', link: '#'},
+        {id: 4, name: 'Contact', link: '#'},
+    ],
+    skillsData: [
+        {id: 1, iconId: 'htmlIcon', name: 'html'},
+        {id: 2, iconId: 'cssIcon', name: 'css'},
+        {id: 3, iconId: 'scssIcon', name: 'scss'},
+        {id: 4, iconId: 'jsIcon', name: 'js'},
+        {id: 5, iconId: 'reactIcon', name: 'react'},
+        {id: 6, iconId: 'reduxIcon', name: 'redux'},
+        {id: 7, iconId: 'gitIcon', name: 'git'},
+        {id: 8, iconId: 'tsIcon', name: 'ts'},
+        {id: 9, iconId: 'scIcon', name: 'sc'},
+        {id: 10, iconId: 'githubIcon', name: 'github'},
+    ],
+    skillsButtonData: ['all', 'landing page', 'react', 'spa'],
+}
