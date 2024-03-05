0ea9fac27b3e19b3dd97be0014d67c000d48f9fc
DePacos <depacos@yandex.ru>
2024-03-05 23:51:40 +0300

    added appPropsType

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/Main";
 
-function App() {
+type appPropsType = {
+    dataBase: {
+        menuData: {
+                id: number;
+                name: string;
+                link: string;
+            }[];
+        skillsData: {
+            id: number;
+            iconId: string;
+            name: string;
+        }[];
+        skillsButtonData: Array<string>,
+    };
+}
+
+function App(props: appPropsType) {
     return (
         <div className="App">
-            <Header/>
-            <Main/>
+            <Header menuData={props.dataBase.menuData}/>
+            <Main
+                skillsData={props.dataBase.skillsData}
+                skillsButtonData={props.dataBase.skillsButtonData}
+            />
         </div>
     );
 }
