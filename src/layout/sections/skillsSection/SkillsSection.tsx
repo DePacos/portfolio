0bcc45ca08eb21ee5646bc8bd84942084cb057ab
DePacos <depacos@yandex.ru>
2024-03-06 00:01:18 +0300

    delete skillItem and added killsSectionPropsType

--- src/layout/sections/skillsSection/SkillsSection.tsx
+++ src/layout/sections/skillsSection/SkillsSection.tsx
@@ -4,39 +4,28 @@ import {StyledTitle} from "../../../components/title/Title";
 import {SkillSimple} from "./skillSimple/SkillSimple";
 import dots from "../../../assets/icon/dots.svg"
 
-const skillItems = [
-    {iconId: 'htmlIcon', name: 'html'},
-    {iconId: 'cssIcon', name: 'css'},
-    {iconId: 'scssIcon', name: 'scss'},
-    {iconId: 'jsIcon', name: 'js'},
-    {iconId: 'reactIcon', name: 'react'},
-    {iconId: 'reduxIcon', name: 'redux'},
-    {iconId: 'gitIcon', name: 'git'},
-    {iconId: 'tsIcon', name: 'ts'},
-    {iconId: 'styledComponents', name: 'sc'},
-    {iconId: 'githubIcon', name: 'github'},
-]
-export function SkillsSection() {
+export type SkillsSectionPropsType = {
+    skillsData: {
+        id: number;
+        iconId: string;
+        name: string;
+    }[],
+}
+
+export function SkillsSection(props:SkillsSectionPropsType) {
     return (
         <StyledSection>
             <StyledTitle>Skills</StyledTitle>
-            <StyledWrapSkills>
-                <SkillSimple skillItems={skillItems}/>
-            </StyledWrapSkills>
+            <SkillSimple
+                skillsData={props.skillsData}
+            />
         </StyledSection>
     );
 }
 
 const StyledSection = styled.section
-`
-    max-width: 1200px;
-    margin: 0 auto;
-    background: url(${dots}) no-repeat left center;
-    
-    padding-bottom: 60px;
-`
-
-const StyledWrapSkills = styled.div
-`
-
-`
+    `
+        max-width: 1200px;
+        margin: 0 auto;
+        background: url(${dots}) no-repeat left center;
+    `
