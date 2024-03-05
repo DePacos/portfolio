84dbfc2657efaccf3c5297f4e1d7752954f866aa
DePacos <depacos@yandex.ru>
2024-03-06 00:00:23 +0300

    added SkillSimplePropsType

--- src/layout/sections/skillsSection/skillSimple/SkillSimple.tsx
+++ src/layout/sections/skillsSection/skillSimple/SkillSimple.tsx
@@ -2,16 +2,24 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../../../components/icons/Icon";
 
-export function SkillSimple(props: { skillItems: Array<any> }) {
+export type SkillSimplePropsType = {
+    skillsData: {
+        id: number;
+        iconId: string;
+        name: string;
+    }[],
+}
+
+export function SkillSimple(props: SkillSimplePropsType) {
 
     return (
         <StyledSkillSimple>
-            {props.skillItems.map((e) => {
+            {props.skillsData.map((e) => {
 
                 let iconSize = '100'
                 let iconViewBox = '0 0 120 120'
 
-                if(e.iconId === 'styledComponents'){iconViewBox = '0 0 38 38'}
+                if(e.iconId === 'scIcon'){iconViewBox = '0 0 38 38'}
                 if(e.iconId === 'tsIcon'){iconViewBox = '-25 -25 170 170'}
                 if(e.iconId === 'jsIcon'){iconViewBox = '-15 -15 150 150'}
                 if(e.iconId === 'githubIcon'){iconViewBox = '0 0 30 30'}
@@ -21,7 +29,7 @@ export function SkillSimple(props: { skillItems: Array<any> }) {
                 if(e.iconId === 'reduxIcon'){iconViewBox = '-5 0 120 120'}
 
                 return (
-                    <li>
+                    <li key={e.id}>
                         <Icon iconId={e.iconId} size={iconSize} viewBox={iconViewBox}/>
                         <span>{e.name}</span>
                     </li>
