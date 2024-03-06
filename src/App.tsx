25257947f9f8809c1b0676907f6a7aa852828c1c
DePacos <depacos@yandex.ru>
2024-03-06 23:11:40 +0300

    refactor appPropsType

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/Main";
+import styled from "styled-components";
+
+export type MenuData = {
+    id: number;
+    name: string;
+    link: string;
+}
+
+export type SkillsData = {
+    id: number;
+    iconId: string;
+    name: string;
+}
+
+export type ProjectData = {
+    id: number;
+    title: string;
+    image: string;
+    link: string;
+}
 
 type appPropsType = {
     dataBase: {
-        menuData: {
-                id: number;
-                name: string;
-                link: string;
-            }[];
-        skillsData: {
-            id: number;
-            iconId: string;
-            name: string;
-        }[];
+        menuData: MenuData[],
+        skillsData: SkillsData[],
+        projectData: ProjectData[],
         skillsButtonData: Array<string>,
-    };
+    }
 }
 
-function App(props: appPropsType) {
+export function App(props: appPropsType) {
     return (
         <div className="App">
-            <Header menuData={props.dataBase.menuData}/>
-            <Main
-                skillsData={props.dataBase.skillsData}
-                skillsButtonData={props.dataBase.skillsButtonData}
-            />
+            <StyledContainer>
+                <Header menuData={props.dataBase.menuData}/>
+                <Main
+                    skillsData={props.dataBase.skillsData}
+                    skillsButtonData={props.dataBase.skillsButtonData}
+                    projectData={props.dataBase.projectData}
+                />
+            </StyledContainer>
         </div>
     );
 }
 
-export default App;
+const StyledContainer = styled.div
+    `
+        max-width: 1440px;
+        margin: 0 auto;
+    `
