3051bac6fcbd8494c0377cb7eb35c7b36fa4f694
DePacos <depacos@yandex.ru>
2024-03-06 23:10:37 +0300

    added object projectData

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,3 +1,7 @@
+import project_1 from  '../assets/images/project-1.webp'
+import project_2 from  '../assets/images/project-2.webp'
+import project_3 from  '../assets/images/project-3.webp'
+import project_4 from  '../assets/images/project-4.webp'
 export const dataBase = {
     menuData: [
         {id: 1, name: 'Home', link: '#'},
@@ -18,4 +22,54 @@ export const dataBase = {
         {id: 10, iconId: 'githubIcon', name: 'github'},
     ],
     skillsButtonData: ['all', 'landing page', 'react', 'spa'],
+    projectData: [
+        {
+            id: 1,
+            title: 'Story motion for sale English cources',
+            image: project_1,
+            link: '#',
+        },
+        {
+            id: 2,
+            title: 'Story motion for sale English cources',
+            image: project_2,
+            link: '#',
+        },
+        {
+            id: 3,
+            title: 'Story motion for sale English cources',
+            image: project_3,
+            link: '#',
+        },
+        {
+            id: 4,
+            title: 'Story motion for sale English cources',
+            image: project_4,
+            link: '#',
+        },
+        {
+            id: 5,
+            title: 'Story motion for sale English cources',
+            image: project_1,
+            link: '#',
+        },
+        {
+            id: 6,
+            title: 'Story motion for sale English cources',
+            image: project_2,
+            link: '#',
+        },
+        {
+            id: 7,
+            title: 'Story motion for sale English cources',
+            image: project_3,
+            link: '#',
+        },
+        {
+            id: 8,
+            title: 'Story motion for sale English cources',
+            image: project_4,
+            link: '#',
+        },
+    ]
 }
