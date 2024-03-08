87fb29f448c256fc890179d7c7e44cbb1c563295
DePacos <depacos@yandex.ru>
2024-03-08 23:28:32 +0300

    added @media to disable the animation

--- src/Styles/GlobalStyles.tsx
+++ src/Styles/GlobalStyles.tsx
@@ -3,18 +3,22 @@ import {glTheme} from "./ThemeStyled";
 
 export const GlobalStyles = createGlobalStyle
 `
-*,
-*::before,
-*::after{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-}
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
 
-*{box-sizing: border-box; color: ${glTheme.colors.font};}
-.App{background-color: ${glTheme.colors.bgBodyDark};}
-a{text-decoration: none}
-ul{list-style: none;}
+    *:focus-visible {outline: 3px solid #b97119;}
+    * {box-sizing: border-box; color: ${glTheme.colors.font};}
+    .App {background-color: ${glTheme.colors.bgBodyDark};}
+    a {text-decoration: none}
+    ul {list-style: none;}
 
+    @media (prefers-reduced-motion: reduce) {
+        animation: none;
+    }
 
 `
