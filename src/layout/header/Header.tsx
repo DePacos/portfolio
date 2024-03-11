aa2a45f9a41ecd6a40ba17fbaf40d2902f2bf103
DePacos <depacos@yandex.ru>
2024-03-11 23:59:05 +0300

    added useEffect and sticky header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
 import {Icon} from "../../components/icons/Icon";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 import {MenuData} from "../../App";
 
@@ -10,9 +10,22 @@ export type HeaderPropsType = {
     menuData: MenuData[];
 }
 
+type StyledHeaderPropsType = {
+    offset: number
+}
+
 export const Header = (props: HeaderPropsType) => {
+
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        window.onscroll = () => {
+            setOffset(window.pageYOffset);
+        };
+    }, []);
+
     return (
-        <StyledHeader>
+        <StyledHeader offset={offset}>
             <StyledContainer>
                 <Logo/>
                 <Menu menuData={props.menuData}/>
@@ -23,6 +36,8 @@ export const Header = (props: HeaderPropsType) => {
     );
 };
 
+
+
 const StyledContainer = styled.div
     `
         display: grid;
@@ -32,9 +47,32 @@ const StyledContainer = styled.div
         max-width: 1360px;
         margin: auto;
         padding: 35px 0 75px 0;
-
+        
     `
-const StyledHeader = styled.header
+const StyledHeader = styled.header<StyledHeaderPropsType>
     `   padding: 0 15px;
-        background: ${({theme}) => theme.colors.bgHeaderDark};
+        background: linear-gradient(90deg,rgba(36,51,92,1) 30%,rgba(34,34,34,1) 30%);
+        position: fixed;
+        z-index: 3;
+        max-width: 1440px;
+        width: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+
+        & > div{
+            transition: all 0.5s;
+        }
+
+    
+    ${props => props.offset > 80 && css <StyledHeaderPropsType>
+        `
+                border-radius: 0 0 20px 20px;
+            & > div{
+                padding: 10px 0;
+            }
+        `
+}
+        
     `
