14eef8d66196c2f78c6b5ccd3e6f3e04131da71f
DePacos <depacos@yandex.ru>
2024-03-18 00:13:18 +0300

    added React.FC for icon Component

--- src/components/icons/Icon.tsx
+++ src/components/icons/Icon.tsx
@@ -6,7 +6,7 @@ type IconPropsType = {
     size?: string,
     viewBox?: string,
 }
-export const Icon = (props: IconPropsType) => {
+export const Icon: React.FC<IconPropsType> = (props: IconPropsType) => {
     return (
         <svg width={props.size || '50'} height={props.size || '50'} viewBox={props.viewBox || '0 0 50 50'} fill="none" xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconsSprite}#${props.iconId}`}/>
