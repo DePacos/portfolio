045d8b8e6fa6b33db160e8f62b4014f35e39039d
DePacos <depacos@yandex.ru>
2024-03-18 00:12:06 +0300

    transfer ThemeProvider in App Component

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import {dataBase} from "./redux/state";
 import {GlobalStyles} from "./Styles/GlobalStyles";
-import {ThemeProvider} from "styled-components";
-import {glTheme} from "./Styles/ThemeStyled";
 
 import './assets/fonts/fonts.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -12,12 +10,13 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={glTheme}>
-            <App  dataBase={dataBase}/>
             <GlobalStyles/>
-        </ThemeProvider>
+            <App dataBase={dataBase}/>
     </React.StrictMode>
 );
+
+
 
