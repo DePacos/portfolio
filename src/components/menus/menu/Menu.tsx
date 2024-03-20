f4d7d696c07e713c642c4b43b4e33c4dedf84b9a
DePacos <depacos@yandex.ru>
2024-03-20 17:22:44 +0300

    added functionality to select a menu when scrolling

--- src/components/menus/menu/Menu.tsx
+++ src/components/menus/menu/Menu.tsx
@@ -8,11 +8,48 @@ type MenuPropsType = {
 }
 
 export const Menu: React.FC<MenuPropsType> = (props: MenuPropsType) => {
+
+    const [activeSection, setActiveSection] = React.useState<string>('');
+
+    const isInViewport = (elem: any) => {
+        const position = elem.getBoundingClientRect()
+        // if (elem.id == 'projects') {
+        //     console.log(
+        //         'id: ', elem.id,
+        //         'position-top:', position.top,
+        //         'position-bottom:', position.bottom,
+        //     )
+        // }
+        return (
+            position.top >= 0 &&
+            position.left >= 0 &&
+            position.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+            // position.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    React.useEffect(() => {
+        const handleScroll = () => {
+            const sections = document.querySelectorAll('section')
+            sections.forEach((section) => {
+                if (isInViewport(section)) {
+                    setActiveSection(section.id);
+                }
+            })
+        }
+        window.addEventListener('scroll', handleScroll)
+        return () => (window.removeEventListener('scroll', handleScroll))
+    }, [])
+
     return (
-            <ul>
-                {props.menuData.map((e) => {
-                    return <li key={e.id}><a href={e.link}>{e.name}</a></li>
-                })}
-            </ul>
+        <ul>
+            {props.menuData.map((e) => {
+                return <li key={e.id}>
+                    <a className={`${e.link}` === activeSection ? 'active' : ''}
+                       href={`#${e.link}`}>
+                        {e.name}
+                    </a></li>
+            })}
+        </ul>
     );
 }
