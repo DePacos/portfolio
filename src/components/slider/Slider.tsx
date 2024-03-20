04304d0843908d2e5d8d99e20fbcdd0501ce4230
DePacos <depacos@yandex.ru>
2024-03-20 17:26:50 +0300

    adding method map for slide

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Carousel} from "react-responsive-carousel";
-import {S} from "./Slider-Styles";
+import {S} from "./Slider_Styles";
 
-import testimonialPhoto from '../../assets/images/testi-face.webp'
-export const Slider: React.FC = () => {
+import {TestimonialData} from "../../App";
+
+type SliderPropsType = {
+    testimonialData: TestimonialData[],
+}
+export const Slider: React.FC<SliderPropsType> = (props: SliderPropsType) => {
     return (
         <Carousel
             showArrows={false}
@@ -13,27 +17,13 @@ export const Slider: React.FC = () => {
             infiniteLoop={true}
             interval={5000}
         >
-            <S.Slide>
-                <img src={testimonialPhoto} alt="Testimonial"/>
-                <span>Michal John</span>
-                <p>Quote testimonials demonstrate support for your product or feature from a user who has experience
-                    with it. This can be significantly more effective than traditional advertising methods as most
-                    consumers will trust.</p>
-            </S.Slide>
-            <S.Slide>
-                <img src={testimonialPhoto} alt="Testimonial"/>
-                <span>Michal John</span>
-                <p>Quote testimonials demonstrate support for your product or feature from a user who has experience
-                    with it. This can be significantly more effective than traditional advertising methods as most
-                    consumers will trust.</p>
-            </S.Slide>
-            <S.Slide>
-                <img src={testimonialPhoto} alt="Testimonial" />
-                <span>Michal John</span>
-                <p>Quote testimonials demonstrate support for your product or feature from a user who has experience
-                    with it. This can be significantly more effective than traditional advertising methods as most
-                    consumers will trust.</p>
-            </S.Slide>
+                {props.testimonialData.map((e) => {
+                    return <S.Slide>
+                        <img src={e.image} alt="Testimonial"/>
+                        <span>{e.name}</span>
+                        <p>{e.testimonial}</p>
+                    </S.Slide>
+                })}
         </Carousel>
     );
 }
